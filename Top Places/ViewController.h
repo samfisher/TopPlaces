6bb57614848e0cc069d31f49ba5a29bc9d8da167
Sam Fisher <samfi@magenic.com>
2015-10-20 13:59:47 -0500

    Initial Commit- Project setup

---
+++ Top Places/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Top Places
+//
+//  Created by Sam Fisher on 10/20/15.
+//  Copyright © 2015 Sam Fisher Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
